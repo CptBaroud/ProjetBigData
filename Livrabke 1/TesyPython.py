d4fe18533fbfe1f7318b0275047e6643fee79c35
CptBaroud <gurvan.seveno@gmail.com>
2020-05-31 19:14:33 +0200

    add security on 6. to avoid duplication

--- Livrabke 1/TesyPython.py
+++ Livrabke 1/TesyPython.py
@@ -5,10 +5,13 @@ client = MongoClient('localhost', 27017)
 db = client['DataProject']
 collection_traffic = db['vehicules']
 
-for trafic in collection_traffic.find():
-    h = round(trafic['num_periode'] / 60)
-    m = trafic['num_periode'] % 60
-    j = trafic['num_jour'] + 1
+if db['vehicules_stamped'].count() <= 0:
+    for trafic in collection_traffic.find():
+        h = round(trafic['num_periode'] / 60)
+        m = trafic['num_periode'] % 60
+        j = trafic['num_jour'] + 1
 
-    db['vehicules_stamped'].insert_one({'num_arete': trafic['num_arete'], 'date': datetime.datetime(2020, 1, j, h, m).strftime("%d/%m/%Y %Hh%Mm"),
-                                                    'nb_vehicules': trafic['nb_vehicules']})
+        db['vehicules_stamped'].insert_one({'num_arete': trafic['num_arete'], 'date': datetime.datetime(2020, 1, j, h, m).strftime("%d/%m/%Y %Hh%Mm"),
+                                                        'nb_vehicules': trafic['nb_vehicules']})
+    else:
+        print('La base est déjà remplie')
